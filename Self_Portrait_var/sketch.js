09a077a5ca7a08b349c181d94009fcb42c850f99
JoseTorralba <31675524+JoseTorralba@users.noreply.github.com>
2017-09-08 19:26:09 -0400

    Initial commit
    Worked on the design of the website.

---
+++ Self_Portrait_var/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(640, 460);
+        
+    var boxColor = color('white')
+  
+    
+    
+    background(0);
+    
+  
+    
+    fill(boxColor);
+    rect(50, 50, 100, 100);
+
+}
+
+
+function draw() {
+
+
+}
