e1522c78eebcb948bdaf40391e3ba5b3ffdbe8b5
JoseTorralba <31675524+JoseTorralba@users.noreply.github.com>
2017-10-29 19:51:37 -0400

    Finished working with functions

--- Assignments/Assignment7/Assignment7_Functions/js/sketch.js
+++ Assignments/Assignment7/Assignment7_Functions/js/sketch.js
@@ -1,8 +1,11 @@
 function setup() {
     var canvas = createCanvas(900, 500);
     canvas.parent('sketch-holder');
-    background('white');
+    background('#f0eeee');
     noStroke();
+
+    textSize(20);
+    text(" Function in a Loop", 30, 30);
 
     var columnNum = 5;
     var rowNum = 1;
@@ -10,28 +13,42 @@ function setup() {
     var rowSize = height / rowNum;
 
     for (var x = 35; x <= width; x += columnSize) {
-        for (var y = 80; y <= height; y += rowSize) {
+        for (var y = 70; y <= height; y += rowSize) {
 
-
-		  myWindow(x, y, 100, 100, 10);
-
+		  myWindow(x, y, 50, 50, 5);
 
         }   // var y
     }   // var x
+
+    textSize(20);
+    text(" Function Size Variations", 30, height/2);
+
+    myWindow(60, 300, 10, 10, 1);
+
+    myWindow(100, 300, 20, 20, 1);
+
+    myWindow(160, 300, 30, 30, 2);
+
+    myWindow(220, 300, 40, 40, 2);
+
+    myWindow(300, 300, 50, 50, 5);
+
+    myWindow(400, 300, 300, 50, 5);
+
+    myWindow(760, 300, 50, 80, 5);
 }
 
 function myWindow(x, y, w, h, m) {
-
-    /* Black Ellipse - work on */
-    ellipse(x + 55,
-            y,
-            w + 10,
-            h + 30)
+    /* Black Ellipse */
+    ellipse(x + m / 2 + w / 2,
+         y,
+         w + m * 1,
+         h);
 
     /* Black Background */
     rect(x,
          y,
-         w + 10,
+         w + m * 1,
          h + h);
 
     /* Yellow Background For Windows */
@@ -67,7 +84,6 @@ function myWindow(x, y, w, h, m) {
          w / 2 - m / 2,
          h - m / 2);
 
-
     /* 2nd Bottom Right Window */
 	rect(x + m + w / 2,
          y + m + h,
@@ -76,13 +92,13 @@ function myWindow(x, y, w, h, m) {
 
     /* Bottom of Window - work on */
     fill('black');
-    rect(x - 5,
-         y + 200,
-         w + 20,
-         h - 90);
+    rect(x - m / 2,
+         y + h * 2,
+         w + m * 2,
+         h / 4);
 
-    rect(x - 10,
-         y + 210,
-         w + 30,
-         h - 80);
+    rect(x - m / 1,
+         y + h * 2.2,
+         w + m * 3,
+         h / 4);
 }
