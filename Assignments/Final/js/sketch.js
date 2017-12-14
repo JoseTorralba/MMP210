002f1f0f83b2508a78e284419c04dc06d6810ddc
JoseTorralba <31675524+JoseTorralba@users.noreply.github.com>
2017-12-14 17:07:11 -0500

    intial commit

--- Assignments/Final/js/sketch.js
+++ Assignments/Final/js/sketch.js
@@ -7,6 +7,7 @@ function Button(characters, x, y, audio) {
     });
 }
 
+/* Hiragana Audio */
 const audio = [
     "audio/a.mp3",
     "audio/i.mp3",
@@ -61,6 +62,7 @@ const audio = [
     "audio/n.mp3"
 ];
 
+/* Hiragana Characters */
 const characters = [
     'あ',
     'い',
@@ -117,6 +119,7 @@ const characters = [
 
 const buttons = [];
 
+/* Buttons */
 var phrase1;
 var phrase2;
 var phrase3;
@@ -124,31 +127,64 @@ var phrase4;
 var phrase5;
 var phrase6;
 
-var hiragana = "Hiragana is the basic Japanese phonetic alphabet. It represents every sound in the Japanese language. Therefore, you can theoretically write everything in hiragana. However, because Japanese is written with no spaces, this will create nearly indecipherable text.";
+/* Hiragana Definition */
+var hiragana = "Hiragana is one of three character sets used in the Japanese language. Each Hiragana character represents particular syllable. Hiragana is used widely to form a sentence by putting them together.";
+
+/* Text Next to Character Chart */
+var upperText = {
+    x: 710,
+    y: 80
+};
+
+var sideText = {
+    x: 660,
+    y: 170
+};
 
 function setup() {
     createCanvas(1400, 900);
-    background(220);
+    background(255);
 
     /* Text Above Character Chart */
     textSize(25);
-    text("a", 710, 80);
-    text("i", 780, 80);
-    text("u", 850, 80);
-    text("e", 920, 80);
-    text("o", 990, 80);
+    text("a", upperText.x, upperText.y);
+    text("i", upperText.x + 70, upperText.y);
+    text("u", upperText.x + 140 , upperText.y);
+    text("e", upperText.x + 210, upperText.y);
+    text("o", upperText.x + 280, upperText.y);
 
     /* Text Side by Character Chart */
-    text("k-", 660, 170);
-    text("s-", 660, 220);
-    text("t-", 660, 270);
-    text("n-", 660, 320);
-    text("h-", 660, 370);
-    text("m-", 660, 420);
-    text("y-", 660, 470);
-    text("r-", 660, 520);
-    text("w-", 660, 570);
-    text("n-", 660, 620);
+    text("k-", sideText.x, sideText.y);
+    text("s-", sideText.x, sideText.y + 50);
+    text("t-", sideText.x, sideText.y + 100);
+    text("n-", sideText.x, sideText.y + 150);
+    text("h-", sideText.x, sideText.y + 200);
+    text("m-", sideText.x, sideText.y + 250);
+    text("y-", sideText.x, sideText.y + 300);
+    text("r-", sideText.x, sideText.y + 350);
+    text("w-", sideText.x, sideText.y + 400);
+    text("n-", sideText.x, sideText.y + 450);
+
+    /* Text */
+    textSize(30);
+    fill('black');
+    text("Hiragana", 100, 50);
+
+    textSize(20);
+    text("*/// = No Character Given", 350, 50);
+
+    textSize(25);
+	text(hiragana, 100, 100, 450, 300);
+
+    /* Phrases */
+    text("Basic Phrases", 100, 350);
+
+  	phrase1 = new Button('おはよう (Good Morning)', 100, 550, "audio/goodmorning.mp3");
+  	phrase2 = new Button('ありがとうございます (Thank You)', 100, 610, "audio/thankyou.mp3");
+  	phrase3 = new Button('ごめんなさい (Sorry)', 100, 670, "audio/sorry.mp3");
+  	phrase4 = new Button('すみません (Excuse Me)', 100, 730, "audio/excuseme.mp3");
+  	phrase5 = new Button('さようなら (Goodbye)', 100, 790, "audio/goodbye.mp3");
+  	phrase6 = new Button('おやすみなさい (Goodnight)', 100, 850, "audio/goodnight.mp3");
 
     /* Chart of Characters */
     var x = 700;
@@ -168,26 +204,4 @@ function setup() {
         if (y > 900) 
             y = 300;        
     } 
-
-    /* Text */
-    textSize(30);
-    fill('black');
-
-    text("Hiragana", 100, 50);
-
-    textSize(25);
-	text(hiragana, 100, 100, 450, 300);
-
-    /* Phrases */
-    text("Basic Phrases", 100, 425);
-
-  	phrase1 = new Button('おはよう (Good Morning)', 100, 600, "audio/goodmorning.mp3");
-  	phrase2 = new Button('ありがとうございます (Thank You)', 100, 650, "audio/thankyou.mp3");
-  	phrase3 = new Button('ごめんなさい (Sorry)', 100, 700, "audio/sorry.mp3");
-  	phrase4 = new Button('すみません (Excuse Me)', 100, 750, "audio/excuseme.mp3");
-  	phrase5 = new Button('さようなら (Goodbye)', 100, 800, "audio/goodbye.mp3");
-  	phrase6 = new Button('おやすみなさい (Goodnight)', 100, 850, "audio/Goodnight.mp3");
-
-
-
 }
